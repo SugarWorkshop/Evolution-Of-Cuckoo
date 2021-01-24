912f4a54195a3fb5067c2372d9f5e885fc0c9abf
Mo_Ink <64246616+Mo-Ink@users.noreply.github.com>
2021-01-24 10:36:56 +0800

    增加矿石生成

--- src/main/java/com/github/mo_ink/eoc/CommonProxy.java
+++ src/main/java/com/github/mo_ink/eoc/CommonProxy.java
@@ -2,6 +2,7 @@ package com.github.mo_ink.eoc;
 
 import com.github.mo_ink.eoc.handler.CraftingHandler;
 import com.github.mo_ink.eoc.handler.EntityHandler;
+import com.github.mo_ink.eoc.handler.WorldGeneratorHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -9,11 +10,11 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class CommonProxy {
     public void preInit(FMLPreInitializationEvent event) {
         EntityHandler.register();
-        new CraftingHandler();
     }
 
     public void init(FMLInitializationEvent event) {
-
+        new CraftingHandler();
+        new WorldGeneratorHandler();
     }
 
     public void postInit(FMLPostInitializationEvent event) {
--- src/main/java/com/github/mo_ink/eoc/handler/WorldGeneratorHandler.java
+++ src/main/java/com/github/mo_ink/eoc/handler/WorldGeneratorHandler.java
@@ -1,4 +1,26 @@
 package com.github.mo_ink.eoc.handler;
 
+import com.github.mo_ink.eoc.worldgen.WorldGeneratorFunnyOre;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.gen.feature.WorldGenerator;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.terraingen.OreGenEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
 public class WorldGeneratorHandler {
+    private static final WorldGenerator generatorFunnyOre = new WorldGeneratorFunnyOre();
+
+    private BlockPos pos;
+
+    public WorldGeneratorHandler() {
+        MinecraftForge.ORE_GEN_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onOreGenPost(OreGenEvent.Post event) {
+        if (!event.getPos().equals(this.pos)) {
+            this.pos = event.getPos();
+            generatorFunnyOre.generate(event.getWorld(), event.getRand(), event.getPos());
+        }
+    }
 }
