da55da184eab8de655c02e89645dd10d547b6a1b
Mo_Ink <64246616+Mo-Ink@users.noreply.github.com>
2021-01-24 08:53:21 +0800

    添加合成表

--- src/main/java/com/github/mo_ink/eoc/CommonProxy.java
+++ src/main/java/com/github/mo_ink/eoc/CommonProxy.java
@@ -1,6 +1,7 @@
 package com.github.mo_ink.eoc;
 
 import com.github.mo_ink.eoc.handler.EntityHandler;
+import com.github.mo_ink.eoc.handler.OreDictionaryHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -8,6 +9,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class CommonProxy {
     public void preInit(FMLPreInitializationEvent event) {
         EntityHandler.register();
+        new OreDictionaryHandler(event);
     }
 
     public void init(FMLInitializationEvent event) {
