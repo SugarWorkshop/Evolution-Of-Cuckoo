0d7468dcb585309c489ef95b2ebf70514820def8
Mo_Ink <2350745751@qq.com>
2021-02-11 08:30:31 +0800

    优化

--- src/main/java/com/github/mo_ink/eoc/handler/EntityHandler.java
+++ src/main/java/com/github/mo_ink/eoc/handler/EntityHandler.java
@@ -72,10 +72,10 @@ public class EntityHandler {
                 Biomes.EXTREME_HILLS_WITH_TREES,
                 Biomes.EXTREME_HILLS_EDGE
         };
-        registerEntitySpawn(EntityMoInk.class, 8, 1, 2, EnumCreatureType.CREATURE, biomes);
-        registerEntitySpawn(EntityZijing.class, 8, 1, 2, EnumCreatureType.CREATURE, biomes);
-        registerEntitySpawn(EntitySmalldew.class, 12, 1, 3, EnumCreatureType.CREATURE, biomes);
-        registerEntitySpawn(EntityNat.class, 16, 1, 4, EnumCreatureType.CREATURE, biomes);
-        registerEntitySpawn(EntityOsIr.class, 16, 1, 4, EnumCreatureType.CREATURE, biomes);
+        registerEntitySpawn(EntityMoInk.class, 8, 1, 3, EnumCreatureType.CREATURE, biomes);
+        registerEntitySpawn(EntityZijing.class, 8, 1, 3, EnumCreatureType.CREATURE, biomes);
+        registerEntitySpawn(EntitySmalldew.class, 12, 1, 4, EnumCreatureType.CREATURE, biomes);
+        registerEntitySpawn(EntityNat.class, 16, 1, 5, EnumCreatureType.CREATURE, biomes);
+        registerEntitySpawn(EntityOsIr.class, 16, 1, 5, EnumCreatureType.CREATURE, biomes);
     }
 }
