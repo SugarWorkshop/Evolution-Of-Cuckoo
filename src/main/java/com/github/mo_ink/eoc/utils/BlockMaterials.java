d245febd65f7c5f214c964b2408a21b104dd7bde
Mo_Ink <2350745751@qq.com>
2021-02-16 20:55:10 +0800

    Update BlockMaterials.java

--- src/main/java/com/github/mo_ink/eoc/utils/BlockMaterials.java
+++ src/main/java/com/github/mo_ink/eoc/utils/BlockMaterials.java
@@ -4,7 +4,6 @@ import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 
 public class BlockMaterials extends Material {
-    public static final Material IRON = (new BlockMaterials(MapColor.IRON)).setRequiresTool();
     public static final Material GOLD = (new BlockMaterials(MapColor.GOLD)).setRequiresTool();
 
     public BlockMaterials(MapColor color) {
