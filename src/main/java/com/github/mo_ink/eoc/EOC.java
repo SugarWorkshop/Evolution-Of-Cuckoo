93cdda97ab26a6a5c9fc7e0728b94b63e3490cf9
Mo_Ink <2350745751@qq.com>
2021-04-18 02:35:20 +0800

    添加brandings

--- src/main/java/com/github/mo_ink/eoc/EOC.java
+++ src/main/java/com/github/mo_ink/eoc/EOC.java
@@ -1,12 +1,18 @@
 package com.github.mo_ink.eoc;
 
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
 import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mod(modid = EOC.MODID, name = EOC.NAME, version = EOC.VERSION)
 public class EOC {
@@ -32,5 +38,12 @@ public class EOC {
     @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
         proxy.postInit(event);
+    }
+
+    @EventHandler
+    public void addBrandings(FMLLoadCompleteEvent e) {
+        List<String> brandings = new ArrayList<>(FMLCommonHandler.instance().getBrandings(true));
+        brandings.add(0, "§bEvolution §cOf §eCuckoo §r" + VERSION);
+        ReflectionHelper.setPrivateValue(FMLCommonHandler.class, FMLCommonHandler.instance(), brandings, "brandings");
     }
 }
