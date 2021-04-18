6c69af05d4c34675c78d8bbb41fb3aee4fb0275e
Mo_Ink <2350745751@qq.com>
2021-04-18 14:28:42 +0800

    移植gui

---
+++ src/main/java/com/github/mo_ink/eoc/network/PacketTestGuiScreen.java
@@ -0,0 +1,33 @@
+package com.github.mo_ink.eoc.network;
+
+import com.github.mo_ink.eoc.client.gui.GuiEOCManual;
+import io.netty.buffer.ByteBuf;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import net.minecraftforge.fml.relauncher.Side;
+
+public class PacketTestGuiScreen implements IMessage {
+
+	@Override
+	public void fromBytes(ByteBuf buf) {
+	}
+
+	@Override
+	public void toBytes(ByteBuf buf) {
+	}
+
+	public static class Handler implements IMessageHandler<PacketTestGuiScreen, IMessage> {
+
+		@Override
+		public IMessage onMessage(PacketTestGuiScreen message, MessageContext ctx) {
+			if(ctx.side == Side.CLIENT) {
+				Minecraft.getMinecraft().addScheduledTask(() -> Minecraft.getMinecraft().displayGuiScreen(new GuiEOCManual()));
+			}
+			return null;
+		}
+
+	}
+
+}
