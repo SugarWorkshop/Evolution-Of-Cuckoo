13a6c32ee22c428ad065d3c2d462f86fdbb70e0a
Mo_Ink <2350745751@qq.com>
2021-04-24 04:32:24 +0800

    添加CuckooLib支持

--- src/main/java/com/github/mo_ink/eoc/handler/GuiHandler.java
+++ src/main/java/com/github/mo_ink/eoc/handler/GuiHandler.java
@@ -1,18 +1,22 @@
 package com.github.mo_ink.eoc.handler;
 
 import com.github.mo_ink.eoc.EOC;
+import com.github.mo_ink.eoc.client.gui.GuiEOCManual;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.IGuiHandler;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 
+import javax.annotation.Nullable;
+
 public class GuiHandler implements IGuiHandler {
-    public static final int GUIEOCManual = 0;
+    public static final int GUIEOCManual = 1;
 
     public GuiHandler() {
         NetworkRegistry.INSTANCE.registerGuiHandler(EOC.instance, this);
     }
 
+    @Nullable
     @Override
     public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
         switch (ID) {
@@ -23,6 +27,7 @@ public class GuiHandler implements IGuiHandler {
         }
     }
 
+    @Nullable
     @Override
     public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
         switch (ID) {
