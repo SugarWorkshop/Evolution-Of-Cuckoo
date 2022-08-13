965f7d1c8789c40469fb47c23eaf0abea227e3ad
SugarMGP <2350745751@qq.com>
2022-08-13 12:11:46 +0800

    说的道理

---
+++ src/main/java/io/github/sugarmgp/eoc/EOC.java
@@ -0,0 +1,7 @@
+package io.github.sugarmgp.eoc;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(EOC.MODID)
+public class EOC {
+    public static final String MODID = "eoc";
+}
---
+++ src/main/java/io/github/sugarmgp/eoc/handler/EventHandler.java
@@ -0,0 +1,7 @@
+package io.github.sugarmgp.eoc.handler;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber()
+public class EventHandler {
+}
