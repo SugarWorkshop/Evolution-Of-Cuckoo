9d1a5e16815c19eaaf22f0469e4a39ee1d03e512
SugarMGP <2350745751@qq.com>
2022-08-13 21:44:06 +0800

    修复滑稽果效果并优化模组介绍显示

--- src/main/java/io/github/sugarmgp/eoc/item/ItemFunnyApple.java
+++ src/main/java/io/github/sugarmgp/eoc/item/ItemFunnyApple.java
@@ -1,21 +1,32 @@
 package io.github.sugarmgp.eoc.item;
 
 import io.github.sugarmgp.eoc.EOC;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
+import net.minecraft.world.World;
 
 public class ItemFunnyApple extends Item {
-    private static final Food food = (new Food.Builder())
+    private static final Food food = new Food.Builder()
             .saturation(0.4f)
             .hunger(5)
-            .effect(() -> new EffectInstance(Effects.REGENERATION, 160, 2), 1)
-            .effect(() -> new EffectInstance(Effects.HEALTH_BOOST, 400, 1), 1)
+            .effect(() -> new EffectInstance(Effects.REGENERATION, 70, 4), 1)
             .setAlwaysEdible()
             .build();
 
     public ItemFunnyApple() {
         super(new Properties().food(food).group(EOC.ITEMGROUP));
+    }
+
+    @Override
+    public ItemStack onItemUseFinish(ItemStack stack, World worldIn, LivingEntity entityLiving) {
+        ItemStack itemstack = super.onItemUseFinish(stack, worldIn, entityLiving);
+        float health = entityLiving.getHealth();
+        entityLiving.addPotionEffect(new EffectInstance(Effects.HEALTH_BOOST, 120, 3));
+        entityLiving.setHealth(health); //防止添加效果后重置血量
+        return itemstack;
     }
 }
