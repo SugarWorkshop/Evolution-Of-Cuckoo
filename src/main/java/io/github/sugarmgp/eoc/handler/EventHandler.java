5a5c35fc1eda37c700f4f88e3f7d49dfc80520ca
SugarMGP <2350745751@qq.com>
2022-08-17 21:15:21 +0800

    添加欢迎消息

--- src/main/java/io/github/sugarmgp/eoc/handler/EventHandler.java
+++ src/main/java/io/github/sugarmgp/eoc/handler/EventHandler.java
@@ -1,7 +1,22 @@
 package io.github.sugarmgp.eoc.handler;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+@Mod.EventBusSubscriber
 public class EventHandler {
+    @SubscribeEvent
+    public static void onPlayerJoin(EntityJoinWorldEvent event) {
+        Entity entity = event.getEntity();
+        if (entity instanceof PlayerEntity && !entity.world.isRemote) {
+            PlayerEntity player = (PlayerEntity) entity;
+            String message = "eoc.welcome";
+            TranslationTextComponent text = new TranslationTextComponent(message, player.getDisplayName());
+            player.sendStatusMessage(text, false);
+        }
+    }
 }
