4792d4b20be2b73dde716a151942409bbb077130
SugarMGP <2350745751@qq.com>
2022-08-19 23:09:51 +0800

    优化NPC模型显示

---
+++ src/main/java/io/github/sugarmgp/eoc/entity/model/ModelNPC.java
@@ -0,0 +1,81 @@
+package io.github.sugarmgp.eoc.entity.model;
+
+import com.google.common.collect.Lists;
+import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.client.renderer.model.ModelRenderer;
+import net.minecraft.entity.LivingEntity;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.List;
+
+@OnlyIn(Dist.CLIENT)
+public class ModelNPC extends BipedModel {
+    public final ModelRenderer bipedLeftArmwear;
+    public final ModelRenderer bipedRightArmwear;
+    public final ModelRenderer bipedLeftLegwear;
+    public final ModelRenderer bipedRightLegwear;
+    public final ModelRenderer bipedBodyWear;
+    private final boolean smallArms;
+    private List<ModelRenderer> modelRenderers = Lists.newArrayList();
+
+    public ModelNPC(float modelSize, boolean smallArmsIn) {
+        super(modelSize, 0.0F, 64, 64);
+        this.smallArms = smallArmsIn;
+        if (smallArmsIn) {
+            this.bipedLeftArm = new ModelRenderer(this, 32, 48);
+            this.bipedLeftArm.addBox(-1.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, modelSize);
+            this.bipedLeftArm.setRotationPoint(5.0F, 2.5F, 0.0F);
+            this.bipedRightArm = new ModelRenderer(this, 40, 16);
+            this.bipedRightArm.addBox(-2.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, modelSize);
+            this.bipedRightArm.setRotationPoint(-5.0F, 2.5F, 0.0F);
+            this.bipedLeftArmwear = new ModelRenderer(this, 48, 48);
+            this.bipedLeftArmwear.addBox(-1.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, modelSize + 0.25F);
+            this.bipedLeftArmwear.setRotationPoint(5.0F, 2.5F, 0.0F);
+            this.bipedRightArmwear = new ModelRenderer(this, 40, 32);
+            this.bipedRightArmwear.addBox(-2.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, modelSize + 0.25F);
+            this.bipedRightArmwear.setRotationPoint(-5.0F, 2.5F, 10.0F);
+        } else {
+            this.bipedLeftArm = new ModelRenderer(this, 32, 48);
+            this.bipedLeftArm.addBox(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, modelSize);
+            this.bipedLeftArm.setRotationPoint(5.0F, 2.0F, 0.0F);
+            this.bipedLeftArmwear = new ModelRenderer(this, 48, 48);
+            this.bipedLeftArmwear.addBox(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, modelSize + 0.25F);
+            this.bipedLeftArmwear.setRotationPoint(5.0F, 2.0F, 0.0F);
+            this.bipedRightArmwear = new ModelRenderer(this, 40, 32);
+            this.bipedRightArmwear.addBox(-3.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, modelSize + 0.25F);
+            this.bipedRightArmwear.setRotationPoint(-5.0F, 2.0F, 10.0F);
+        }
+
+        this.bipedLeftLeg = new ModelRenderer(this, 16, 48);
+        this.bipedLeftLeg.addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, modelSize);
+        this.bipedLeftLeg.setRotationPoint(1.9F, 12.0F, 0.0F);
+        this.bipedLeftLegwear = new ModelRenderer(this, 0, 48);
+        this.bipedLeftLegwear.addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, modelSize + 0.25F);
+        this.bipedLeftLegwear.setRotationPoint(1.9F, 12.0F, 0.0F);
+        this.bipedRightLegwear = new ModelRenderer(this, 0, 32);
+        this.bipedRightLegwear.addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, modelSize + 0.25F);
+        this.bipedRightLegwear.setRotationPoint(-1.9F, 12.0F, 0.0F);
+        this.bipedBodyWear = new ModelRenderer(this, 16, 32);
+        this.bipedBodyWear.addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, modelSize + 0.25F);
+        this.bipedBodyWear.setRotationPoint(0.0F, 0.0F, 0.0F);
+    }
+
+    public void setRotationAngles(LivingEntity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+        super.setRotationAngles(entityIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
+        this.bipedLeftLegwear.copyModelAngles(this.bipedLeftLeg);
+        this.bipedRightLegwear.copyModelAngles(this.bipedRightLeg);
+        this.bipedLeftArmwear.copyModelAngles(this.bipedLeftArm);
+        this.bipedRightArmwear.copyModelAngles(this.bipedRightArm);
+        this.bipedBodyWear.copyModelAngles(this.bipedBody);
+    }
+
+    public void setVisible(boolean visible) {
+        super.setVisible(visible);
+        this.bipedLeftArmwear.showModel = visible;
+        this.bipedRightArmwear.showModel = visible;
+        this.bipedLeftLegwear.showModel = visible;
+        this.bipedRightLegwear.showModel = visible;
+        this.bipedBodyWear.showModel = visible;
+    }
+}
